22b2ecfb2bc1a6d8eff910fc51485ed65fb18552
sikanderv <sikanderv@gmail.com>
2018-04-02 14:46:48 -0500

    Fix issue with backdrop on initial App load

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -10,7 +10,7 @@ import { connect } from 'react-redux';
 class Layout extends Component {
 
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
